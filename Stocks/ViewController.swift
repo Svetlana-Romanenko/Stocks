78beec5f86bf433045c1bf0ef1d5a2c80c7443ac
Svetlana <svetaromanenko@inbox.ru>
2021-01-31 20:19:30 +0300

    Initial Commit

---
+++ Stocks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by Светлана Романенко on 31.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
