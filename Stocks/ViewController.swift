3fc43a766eb8caf3f6ff6429496a581376d175e4
Svetlana <svetaromanenko@inbox.ru>
2021-01-31 21:37:01 +0300

    Work with main storyboard, view

--- Stocks/ViewController.swift
+++ Stocks/ViewController.swift
@@ -8,12 +8,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //UI
+    @IBOutlet var companyNameLabel: UILabel!
+    @IBOutlet var companyPickerView: UIPickerView!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
+    //Private
+    private lazy var companies = [
+        "Apple": "AAPL",
+        "Microsoft": "MSFT",
+        "Google": "GOOG",
+        "Amazon": "AMZN",
+        "Facebook": "FB"
+    ]
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        companyNameLabel.text = "Tinkoff"
+        companyPickerView.dataSource = self
+        companyPickerView.delegate = self
+        
+        activityIndicator.startAnimating()
+        requestQuote(for: "AAPL")
     }
-
-
 }
 
+// MARK: - UIPickerViewDataSource
+
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return companies.keys.count
+    }
+    
+    // MARK: - Private
+    
+    private func requestQuote(for symbol: String) {
+        let token = "pk_5949bbf8a9a5406387e6e322dd2f1c1b "
+        
+        guard let url = URL(string: "https://cloud.iexapis.com/stable/stock/\(symbol)/quote?token=\(token)") else {
+            return
+        }
+        
+        let dataTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data {
+                print(data)
+            }
+        }
+        
+        dataTask.resume()
+    }
+}
+
+// MARK: - UIPickerViewDelegate
+
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return Array(companies.keys)[row]
+    }
+}
